2e1114f7d731e8341ce8239f0411f4f5dc7941f9
malicopter <malic.s@gmx.de>
2025-12-30 13:04:41 +0100

    Most movement rules implemented also en passant :D

--- src/core/board.cpp
+++ src/core/board.cpp
@@ -1,6 +1,8 @@
 #include "board.h"
+#include <algorithm>
 #include <cctype>
 #include <cstdlib>
+#include <iostream>
 
 
 bool isLower(unsigned char c) {
@@ -14,10 +16,9 @@ int manhattanDistance(int pieceX, int pieceY, int goalX, int goalY) {
     return std::abs(goalX - pieceX) + std::abs(goalY - pieceY);
 };
 
-
-bool Board::isLegalMove(int pieceX, int pieceY, int goalX, int goalY) {
-    unsigned char piece = boardState[pieceY][pieceX];
-    unsigned char goalPiece = boardState[goalY][goalX];
+bool Board::isLegalMove(int xPiece, int yPiece, int xGoal, int yGoal) {
+    unsigned char piece = boardState[yPiece][xPiece];
+    unsigned char goalPiece = boardState[yGoal][xGoal];
     unsigned char pieceType = std::tolower(piece);
     
     bool pieceIsBlack = isLower(piece);
@@ -25,10 +26,12 @@ bool Board::isLegalMove(int pieceX, int pieceY, int goalX, int goalY) {
     bool goalIsBlack = isLower(goalPiece);
     bool goalIsWhite = !goalIsBlack;
     
-    bool isDifX = (pieceX != goalX);
-    bool isDifY = (pieceY != goalY);
-    int xDif = std::abs(goalX - pieceX);
-    int yDif = std::abs(goalY - pieceY);
+    bool isDifX = (xPiece != xGoal);
+    bool isDifY = (yPiece != yGoal);
+    int xDist = std::abs(xGoal - xPiece);
+    int yDist = std::abs(yGoal - yPiece);
+    int changeX = std::max(std::min(xGoal - xPiece, 1), -1);
+    int changeY = std::max(std::min(yGoal - yPiece, 1), -1);
     
     if (piece == '.') return false; // moving an empty square?
     
@@ -40,48 +43,148 @@ bool Board::isLegalMove(int pieceX, int pieceY, int goalX, int goalY) {
     
     if (!isDifX && !isDifY) return false; // piece captures itself
     
+    if (enPassantPossible && activePlayer == enPassantPlayer) {
+        enPassantPossible = false;
+    }
     
-    if (pieceType == 'k') { // king
-        if (xDif > 1 || yDif > 1) // moved further than 1 square
+    switch (pieceType) {
+        // king
+        case 'k':
+        if (xDist > 1 || yDist > 1) // moved further than 1 square
             return false;
-    } else if (pieceType == 'r') { // rook
+        break;
+        
+        // rook
+        case 'r':
+        // if y and x have changed -> not orthogonal
         if (isDifX && isDifY) {
             return false;
         }
-        if (pieceX != goalX) {
-            if (pieceX < goalX) {
-                for (int i = pieceX + 1; i < goalX; ++i) {
-                    if (boardState[pieceY][i] != '.') {
-                        return false;
-                    }
-                }
-            } else {
-                for (int i = pieceX - 1; i > goalX; --i) {
-                    if (boardState[pieceY][i] != '.') {
-                        return false;
-                    }
+        // moved left / right
+        if (changeX != 0) {
+            for (int x = xPiece + changeX; x != xGoal; x += changeX) {
+                if (boardState[yPiece][x] != '.') {
+                    return false;
                 }
             }
-        } else {
-            if (pieceY < goalY) {
-                for (int i = pieceY + 1; i < goalY; ++i) {
-                    if (boardState[i][pieceX] != '.') {
-                        return false;
-                    }
-                }
-            } else {
-                for (int i = pieceY - 1; i > goalY; --i) {
-                    if (boardState[i][pieceX] != '.') {
-                        return false;
-                    }
+        } // moved up / down
+        else {
+            for (int y = yPiece; y != yGoal; y += changeY) {
+                if (boardState[y][xPiece] != '.') {
+                    return false;
                 }
             }
         }
+        break;
+            
+        // bishop
+        case 'b':
+        // not diagonal
+        if (xDist != yDist) {
+            return false;
+        }
+        for (int i = 1; i < xDist; ++i) {
+            if (boardState[yPiece + i * changeY][xPiece + i * changeX] != '.') {
+                return false;
+            }
+        }
+        break;
+            
+        // queen
+        case 'q':
+        if (isDifX && isDifY) {
+            if (xDist != yDist) {
+                return false;
+            }
+            for (int i = 1; i < xDist; ++i) {
+                if (boardState[yPiece + i * changeY][xPiece + i * changeX] != '.') {
+                    return false;
+                }
+            }
+        } else if (changeY == 0) {
+            for (int x = xPiece + changeX; x != xGoal; x += changeX) {
+                if (boardState[yPiece][x] != '.') {
+                    return false;
+                }
+            }
+        } else {
+            for (int y = yPiece + changeY; y != yGoal; y += changeY) {
+                if (boardState[y][xPiece] != '.') {
+                    return false;
+                }
+            }
+        }
+        break;
+        
+        // knight
+        case 'n':
+        if (xDist + yDist != 3 || xDist > 2 || yDist > 2) {
+            return false;
+        }
+        break;
+        
+        // pawn (oh no)
+        case 'p':
+        if (xDist > 1 || yDist > 2 || changeY == 0) {
+            return false;
+        }
+        if ((changeX != 0 && boardState[yGoal][xGoal] == '.' && !enPassantPossible) || 
+            (changeX != 0 && yDist != 1)) {
+            return false;
+        }
+        
+        if (pieceIsWhite) {
+            if (changeY != -1) {
+                return false;
+            }
+            // so like the board is counted from top to bottom, and 6 would be
+            // the 7th row, where the pawn can move 2 squares
+            if (yPiece < 6 && yDist > 1) {
+                return false;
+            }
+        } else {
+            if (changeY != 1) {
+                return false;
+            }
+            // so like the board is counted from top to bottom, and 2 would be
+            // the 3rd row, where the pawn can move 2 squares
+            if (yPiece > 2 && yDist > 1) {
+                return false;
+            }
+        }
+        
+        // wants to en passant
+        if (changeX != 0 && boardState[yGoal][xGoal] == '.') {
+            if (yGoal - changeY != enPassantY || xGoal != enPassantX) {
+                return false;
+            } else {
+                boardState[enPassantY][enPassantX] = '.';
+            }
+        }
+        
+        if (yDist == 2) {
+            enPassantX = xGoal;
+            enPassantY = yGoal;
+            enPassantPlayer = activePlayer;
+            enPassantPossible = true;
+        }
+        break;
+        
     }
     
     
-    boardState[pieceY][pieceX] = '.';
-    boardState[goalY][goalX] = piece;
+    boardState[yPiece][xPiece] = '.';
+    boardState[yGoal][xGoal] = piece;
     activePlayer = !activePlayer;
+    return true;
+}
+
+bool Board::kingMove(int pieceX, int pieceY, int goalX, int goalY) {
+    int xDif = std::abs(goalX - pieceX);
+    int yDif = std::abs(goalY - pieceY);
+    
+    if (xDif > 1 || yDif > 1) {
+        return false;
+    }
     return true;
 }
--- src/core/board.h
+++ src/core/board.h
@@ -20,14 +20,27 @@ public:
     
     bool activePlayer = 1; // 1 is white and 0 is black
     int gameState = 0;
-    // Castle stuff
-    bool kingMoved = false, leftRookMoved = false, rightRookMoved = false;
+    // in order:
+    // white: king moved 1 rook moved, 8 rook moved,
+    // black: king moved, 1 rook moved, 8 rook moved
+    bool piecesHaveMoved[6] = {0};
     
     // if it is 50, its a draw
     int movesWithoutCapture = 0;
     
+    int enPassantX = 0, enPassantY = 0;
+    bool enPassantPlayer = 0;
+    bool enPassantPossible = false;
+    
     bool isLegalMove(int pieceX, int pieceY, int goalX, int goalY);
     
+private:
+    bool kingMove(int pieceX, int pieceY, int goalX, int goalY);
+    bool rookMove(int pieceX, int pieceY, int goalX, int goalY);
+    bool bishopMove(int pieceX, int pieceY, int goalX, int goalY);
+    bool pawnMove(int pieceX, int pieceY, int goalX, int goalY);
+    bool knightMove(int pieceX, int pieceY, int goalX, int goalY);
+    bool queenMove(int pieceX, int pieceY, int goalX, int goalY);
 };
 
 #endif
